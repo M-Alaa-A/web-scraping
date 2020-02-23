a6e4bfaab5c076960711ac4f3c64473160ea59fe
M-Alaa-A <mla132455@gmail.com>
2020-02-23 23:19:42 +0200

    Add frontendand requirements file

--- wws/myapp/views.py
+++ wws/myapp/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
 
 def home(request):
     return render(request, 'base.htm')
+def newsearch(request):
+    search = request.POST.get('searchofinput')
+    stuff_for_frontend = {
+        'search':search,
+        } 
+    return render(request, 'myapp/newsearch.htm', stuff_for_frontend)  #you must put 'search' first before search
